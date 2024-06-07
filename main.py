3f553cebb938808c58007e260e33dddc9764fd9d
Yullia <Yuliia.valentine@gmail.com>
2024-06-07 22:30:03 +0300

    Handle non-alphabetic characters

--- main.py
+++ main.py
@@ -3,6 +3,14 @@ import pandas
 nato_dataframe = pandas.read_csv("nato_phonetic_alphabet.csv")
 nato_dict = {row.letter: row.code for (index, row) in nato_dataframe.iterrows()}
 
-word = input("Please, enter a word: ").upper()
-output_list = [nato_dict[letter] for letter in word]
-print(output_list)
+word = None
+
+while word == None:
+    word = input("Please, enter a word: ").upper()
+    try:
+        output_list = [nato_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please")
+        word = None
+    else:
+        print(output_list)
